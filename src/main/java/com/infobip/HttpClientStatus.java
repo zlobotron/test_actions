e260d77ba774113e46178fc05a3b53a0cf2fad72
Timofey Kapranov <Timofey.Kapranov@infobip.com>
2023-09-14 11:55:36 +0300

    first commit

---
+++ src/main/java/com/infobip/HttpClientStatus.java
@@ -0,0 +1,25 @@
+package com.infobip;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class HttpClientStatus {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://google.com/"))
+                .GET() // GET is default
+                .build();
+
+        HttpResponse<Void> response = client.send(request,
+                HttpResponse.BodyHandlers.discarding());
+
+        System.out.println(response.statusCode());
+    }
+
+}
